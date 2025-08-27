daccafcbf6944f99dac5081bc2f5530a1b1b0798
Vladyslav Florko <florkovlad@email.com>
2025-08-27 16:17:09 +0300

    зробив задачі

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,14 @@
-function getElementWidth(content, padding, border,) {
-  const contentNum = parseFloat(content);
-  const paddingNum = parseFloat(padding);
-  const borderNum = parseFloat(border);
-
- 
-  const totalWidth = contentNum + paddingNum * 2 + borderNum * 2;
-  return totalWidth;
-
+function filterArray(numbers, value) {
+  const filtrNambers = [];
+  for (const number of numbers) {
+    if (number > value) {
+      filtrNambers.push(number);
+    }
+  }
+  return filtrNambers;
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
